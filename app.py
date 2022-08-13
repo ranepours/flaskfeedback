f3b8d55fbbd6b037228f651736e2a22109ca53b3
Rane Jasia <ranedavis@ranepours.attlocal.net>
2022-08-12 21:11:02 -0400

    idk whats happening i need a nap

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, redirect
-from secrets import SECRET
+from flask import Flask, render_template, redirect, session
 from models import connect_db, db, User, Feedback
+from secrets import Key
+from forms import UserForm, FeedbackForm, RegisterForm, LoginForm
+from werkzeug.exceptions import Unauthorized
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flaskfdbk'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = SECRET
+app.config['SECRET_KEY'] = Key
 
 connect_db(app)
 db.create_all()
@@ -15,20 +17,78 @@ db.create_all()
 @app.route('/')
 def redirect_to():
     """redirect to register page"""
-    return redirect()
+    return redirect('/register')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """render and handle register form"""
-    return
+    form = RegisterForm()
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+    if form.validate_on_submit():
+        username=form.username.data
+        password=form.password.data
+        first_name=form.first_name.data
+        last_name=form.last_name.data
+        email=form.email.data
+
+        user=User.register(username,password,first_name,last_name,email)
+        db.session.commit()
+        session['username']=user.username
+        return redirect(f'/users/{user.username}')
+    else:
+        return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def logins():
     """render and handle login form"""
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        username=form.username.data
+        password=form.password.data
+
+        user=User.authenticate(username,password)
+        if user:
+            session['username']=user.username
+            return redirect(f"/users/{user.username}")
+        else:
+            form.username.errors=['Invalid username/password']
+            return render_template("login.html", form=form)
+    return render_template('login.html', form=form)
+
+@app.route('/users/<username>')
+def render_user(username):
+    """render user page"""
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+    user=User.query.get(username)
+    return render_template("render.html", user=user)
+
+@app.route('/logout')
+def logout():
+    """handle logouts"""
+    session.pop('username')
+    return redirect('/login')
+
+@app.route('/users/<username>/delete', methods=["POST"])
+def delete_user():
+    """remove user from db"""
     return
 
-@app.route('/secret')
-def secrets():
-    return "<h1>You made it</h1>"
+@app.route('/users/<username>/feedback/add', methods=["GET", "POST"])
+def handle_new():
+    """handle new feedback"""
+    return 
 
-#feedback routes
+@app.route('/feedback/<int:feedback_id>/update', methods=['GET', 'post'])
+def handle_update():
+    """handle updates to feedback"""
+    return
+
+@app.route('/feedback/<int:feedback_id>/delete', methods=["POST"])
+def delete_feedback(feedback_id):
+    """remove feedback post"""
+    return
--- forms.py
+++ forms.py
@@ -1,10 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length, Email
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Length(max=20)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=8)])
+    email = StringField("Email", validators=[InputRequired(), Email(), Length(max=50)])
+    first_name=StringField("First Name", validators=[InputRequired(), Length(max=30)])
+    last_name=StringField("Last Name", validators=[InputRequired(), Length(max=30)])
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Length(max=20)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=8)])
 
 class UserForm(FlaskForm):
     username=StringField("Username", validators=[InputRequired()])
     password=PasswordField("Password", validators=[InputRequired()])
 
 class FeedbackForm(FlaskForm):
-    text = StringField("Feedback Text", validators=[InputRequired()])
+    content = StringField("Feedback Text", validators=[InputRequired()])
+    title = StringField("Title", validators=[InputRequired(), Length(max=100)])
